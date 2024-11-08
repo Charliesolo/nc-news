eb1ea82a00fb5b3f61345a922936334e1d42c8f9
Charliesolo <charliesolomon99@googlemail.com>
2024-11-08 10:07:17 +0000

    fix voting system

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -11,73 +11,71 @@ function Votes({votes, id, articlesOrComments}) {
     const[downVoteClass, setDownVoteClass] = useState('notClicked')
     const[error, setError] = useState(null)
 
-    useEffect(()=> {
-        if(upVoteClicked){setUpVoteClass('clicked')}
-        else {setUpVoteClass('notClicked')}
-        if(downVoteClicked){setDownVoteClass('clicked')}
-        else {setDownVoteClass('notClicked')}
-        setVotes(id, articlesOrComments, 0)
-        .then((votes)=> {
-            setCurrentVotes(votes)
-        })
-    }, [currentVotes])
-
-function handelUpVote(){
+function handleUpVote(){
+    let voteChange = 1
     if(downVoteClicked){
-        handelDownVote()
-    }
-    if(upVoteClicked){
-        setCurrentVotes( currentVotes - 1)
-        setUpVoteClicked(false)
-        setVotes(id, articlesOrComments, -1).then(()=> {
-            setError(null)
-        })
-        .catch((error) => {
-        setError(error) 
-        setCurrentVotes(currentVotes)   
-        })
-    }
-    else {
-        setCurrentVotes( currentVotes + 1)
-        setUpVoteClicked(true)
-        setVotes(id, articlesOrComments, 1).then(()=> {
-            setError(null)
-        })
-        .catch((error) => {
-        setError(error) 
-        setCurrentVotes(currentVotes)   
-        })
-    }
-}
-
-function handelDownVote(){
-    if(upVoteClicked){
-        handelUpVote()
-    }
-    if(downVoteClicked){
-        setCurrentVotes( currentVotes + 1)
+        voteChange= 2
         setDownVoteClicked(false)
-        setVotes(id, articlesOrComments, + 1)
-        .catch((error) => {
-            setError(error)
-            setCurrentVotes(currentVotes)
-    })        
+        setDownVoteClass('notClicked') 
     }
-    else {
-        setCurrentVotes( currentVotes - 1)
-        setDownVoteClicked(true)
-        setVotes(id, articlesOrComments, -1)
-        .catch((error) => {
-            setError(error)
-            setCurrentVotes(currentVotes)
+    if(upVoteClicked){
+        voteChange = -1
+        setUpVoteClicked(false)
+        setUpVoteClass('notClicked')
+    }
+    setCurrentVotes( currentVotes + voteChange)
+    if(upVoteClicked){
+        setUpVoteClicked(false)
+        setUpVoteClass('notClicked')       
+    }
+    else{
+        setUpVoteClicked(true)
+        setUpVoteClass('clicked')
+    }
+    setVotes(id, articlesOrComments, voteChange).then(()=> {
+        setError(null)
     })
-    }
+    .catch((error) => {
+    setError(error) 
+    setCurrentVotes(currentVotes)   
+    })    
 }
+
+function handleDownVote(){
+    let voteChange = -1
+    if(upVoteClicked){
+        voteChange= -2
+        setUpVoteClicked(false)
+        setUpVoteClass('notClicked') 
+    }
+    if(downVoteClicked){
+        voteChange = 1
+        setDownVoteClicked(false)
+        setDownVoteClass('notClicked')
+    }
+    setCurrentVotes( currentVotes + voteChange)
+    if(downVoteClicked){
+        setDownVoteClicked(false)
+        setDownVoteClass('notClicked')       
+    }
+    else{
+        setDownVoteClicked(true)
+        setDownVoteClass('clicked')
+    }
+    setVotes(id, articlesOrComments, voteChange).then(()=> {
+        setError(null)
+    })
+    .catch((error) => {
+    setError(error) 
+    setCurrentVotes(currentVotes)   
+    })    
+}
+
 
   return (
       <div><h4>Votes: {currentVotes}</h4>      
-    <button onClick={handelUpVote} className={upVoteClass} >+1</button>
-    <button onClick={handelDownVote} className={downVoteClass} >-1</button>
+    <button onClick={handleUpVote} className={upVoteClass} >+1</button>
+    <button onClick={handleDownVote} className={downVoteClass} >-1</button>
     {error? <p>Your vote was not successful please try again</p>: null}
     </div>
   )
